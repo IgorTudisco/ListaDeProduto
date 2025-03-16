248409c37e1d8a299fb906d2f5e5f04eb354b130
Igor Tudisco <igortudisco@hotmail.com>
2025-03-16 17:03:29 -0300

    Atividade Ag. 3

--- MauiAppMinhasCompras/MauiAppMinhasCompras/App.xaml.cs
+++ MauiAppMinhasCompras/MauiAppMinhasCompras/App.xaml.cs
@@ -1,7 +1,33 @@
-﻿namespace MauiAppMinhasCompras
+﻿using MauiAppListaDeCompras.Helpes;
+
+namespace MauiAppMinhasCompras
 {
     public partial class App : Application
     {
+        private static SQLiteDatabaseHelper _databaseHelper;
+
+        public static SQLiteDatabaseHelper DatabaseHelper
+        {
+            get
+            {
+                if (_databaseHelper == null)
+                {
+                    string path = Path.Combine
+                        (
+                            Environment.GetFolderPath
+                            (
+
+                                Environment.SpecialFolder.LocalApplicationData
+
+                            ), "db_compras.db3"
+                        );
+
+                    _databaseHelper = new SQLiteDatabaseHelper(path);
+                }
+                return _databaseHelper;
+            }
+        }
+
         public App()
         {
             InitializeComponent();
--- MauiAppMinhasCompras/MauiAppMinhasCompras/Views/NovoProduto.xaml.cs
+++ MauiAppMinhasCompras/MauiAppMinhasCompras/Views/NovoProduto.xaml.cs
@@ -1,9 +1,33 @@
+using MauiAppListaDeCompras.Models;
+using System.Threading.Tasks;
+
 namespace MauiAppMinhasCompras.Views;
 
 public partial class NovoProduto : ContentPage
 {
-	public NovoProduto()
-	{
-		InitializeComponent();
-	}
+    public NovoProduto()
+    {
+        InitializeComponent();
+    }
+
+    private async void ToolbarItem_Clicked(object sender, EventArgs e)
+    {
+        try
+        {
+            Produto produto = new Produto
+            {
+                Descricao = txt_descricao.Text,
+                Quantidade = Convert.ToDouble(txt_quantidade.Text),
+                Preco = Convert.ToDouble(txt_preco.Text)
+            };
+
+            await App.DatabaseHelper.Insert(produto);
+
+            await DisplayAlert("Sucesso", "Produto inserido com sucesso", "OK");
+        }
+        catch (Exception ex)
+        {
+            await DisplayAlert("Erro", ex.Message, "OK");
+        }
+    }
 }
